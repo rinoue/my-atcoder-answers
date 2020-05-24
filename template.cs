f3ba4bcf948be22ad70c91726b25e6f2fda7874e
rinoue <rinoue@lares.dti.ne.jp>
2020-05-25 00:05:55 +0900

    fix template

--- template.cs
+++ template.cs
@@ -3,23 +3,19 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-using System.IO;
-using System.Text;
-using System.Globalization;
-
 public class AtCoder
 {
   public static void Main()
   {
-    var S = Console.ReadLine().Trim();
-    var splited = Console.ReadLine().Trim().Split();
-    var splited = Console.ReadLine().Trim().Split().Select(e => int.Parse(e)).ToArray();
-    var a = splited[0];
-    var b = splited[1];
-    var c = splited[2];
-    var d = splited[3];
-    var M = int.Parse(Console.ReadLine().Trim());
-    var N = int.Parse(Console.ReadLine().Trim());
+    var s = Console.ReadLine().Trim();
+    var input = Console.ReadLine().Trim().Split();
+    var input = Console.ReadLine().Trim().Split().Select(e => int.Parse(e)).ToArray();
+    var a = input[0];
+    var b = input[1];
+    var c = input[2];
+    var d = input[3];
+    var m = int.Parse(Console.ReadLine().Trim());
+    var n = int.Parse(Console.ReadLine().Trim());
 
 
     var ans = "";
